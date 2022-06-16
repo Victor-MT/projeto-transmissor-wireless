71ae14807a88c3878e59652e277a8626a71a5f94
Victor Hugo MT <38713423+Victor-MT@users.noreply.github.com>
2022-06-15 22:08:52 -0300

    adding new info

--- src/firmware/receiver/receiver.ino
+++ src/firmware/receiver/receiver.ino
@@ -7,14 +7,23 @@
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 
+/* ---------------------------- LIBRARY IMPORTATION ------------------------- */
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include<string.h>
 
-RF24 radio(7, 8); // CE, CSN
+/* ----------------------- GLOBAL VARIABLES DECLARATION ------------------------ */
+#define STR_MAX_LEN 32 // max message length for nrf24l01 transmitter
+const byte address[6] = "00001"; // channel address
+const char EndMessageSymbol[3] = "#$#"; // End Message Symbol
+RF24 radio(7, 8); // CE pin AND  CSN pin
 
-const byte address[6] = "00001";
+/* ------------------------------- METHODS DECLARATION ------------------------ */
+String readTxMessage();
+bool finishedTransmission(String msg);
 
+/* ------------------------------------ MAIN CODE --------------------------- */
 void setup() {
   Serial.begin(9600);
   radio.begin();
@@ -24,9 +33,47 @@ void setup() {
 }
 
 void loop() {
+  String message;
   if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    Serial.println("STARTING READING MESSAGE...");
+    message = readTxMessage();
   }
+}
+
+/* We need to get each piece of message sent (with size of 32 bytes) 
+   and concatenate it to understand the full message */
+String readTxMessage(){
+  String message = "";
+  char text[STR_MAX_LEN] = "";
+  do{
+    strcpy(text, "");
+    radio.read(&text, sizeof(text));
+    if(!finishedTransmission(EndMessageSymbol, text)){
+      message.concat(String(text));
+    }
+  }while(finishedTransmission(EndMessageSymbol, text));
+  
+  Serial.println("MESSAGE RECEIVED...");
+  Serial.println(message);
+  Serial.println("#################### FINISHED TRANSMISSION ####################");
+  return message;
+}
+
+/* Detect if the transmission was finished through the end message symbol*/
+bool finishedTransmission(char *ems, char *msg){
+  bool isFinished = false;
+  int cmp;
+  
+  char str1[STR_MAX_LEN];
+  char str2[STR_MAX_LEN];
+
+  strcpy(str1, ems);
+  strcpy(str2, msg);
+
+  cmp = strcmp(str1, str2);
+  
+  if(cmp == 0){
+    isFinished = true;
+  }
+  return isFinished;
 }
--- src/firmware/transmistter/transmistter.ino
+++ src/firmware/transmistter/transmistter.ino
@@ -7,14 +7,27 @@
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 
+
+
+/* ---------------------------- LIBRARY IMPORTATION ------------------------- */
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include<string.h>
 
-RF24 radio(7, 8); // CE, CSN
+/* ----------------------- GLOBAL VARIABLES DECLARATION ------------------------ */
+#define STR_MAX_LEN 32 // max message length for nrf24l01 transmitter
+const char EndMessageSymbol[] = "#$#"; // End Message Symbol
+RF24 radio(7, 8); // CE pin AND  CSN pin
+const byte address[6] = "00001"; // channel address
 
-const byte address[6] = "00001";
+/* 500kB message to be sent */
+const String message = "assaasaasasasasasasaasasaassaasassasaasassaassaasassaassaassasassa";
 
+/* ------------------------------- METHODS DECLARATION ------------------------ */
+void sendMessage(String msg);
+
+/* ------------------------------------ MAIN CODE --------------------------- */
 void setup() {
   radio.begin();
   radio.openWritingPipe(address);
@@ -23,7 +36,20 @@ void setup() {
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+  sendMessage(message);
   delay(1000);
+}
+
+/*Due to data size limit of 32 bytes per message from the hardware specification. 
+  This function will split the main message into small packages of 32 bytes and send them to Receiver*/
+void sendMessage(String msg){
+  for (unsigned i = 0; i < msg.length(); i += STR_MAX_LEN) {
+    String subString = msg.substring(i, i + STR_MAX_LEN);
+    char text[STR_MAX_LEN];
+    subString.toCharArray(text, STR_MAX_LEN);
+    
+    radio.write(&text, sizeof(text));
+    delay(10);
+  }
+  radio.write(&EndMessageSymbol, sizeof(EndMessageSymbol)); //to finish the communication, we send the end message symbol
 }
